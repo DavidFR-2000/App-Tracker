acb6f113733172a028b2bbad62eb9d23e38d874a
DavidFR-2000 <lolmanster241@gmail.com>
2024-06-08 20:55:22 +0200

    tailwind commit

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,11 @@ import preact from '@preact/preset-vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/main.jsx',
-            refresh: true,
+            input: [
+                'resources/css/app.css',   // Incluir el archivo CSS principal
+                'resources/js/main.jsx'    // Incluir el archivo JS/JSX principal
+            ],
+            refresh: true,                 // Habilitar recarga automática
         }),
         preact(),
     ],
@@ -17,6 +20,6 @@ export default defineConfig({
             'react/jsx-runtime': 'preact/jsx-runtime',
             'react/jsx-dev-runtime': 'preact/jsx-dev-runtime'
         },
-        extensions: ['.js', '.jsx'] // Asegúrate de que Vite reconoce las extensiones .jsx
+        extensions: ['.js', '.jsx']        // Asegúrate de que Vite reconoce las extensiones .jsx
     }
 });
